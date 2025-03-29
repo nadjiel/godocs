32df0ce567ba22262185b5aec61ca16453562647
Daniel de Oliveira <97067805+nadjiel@users.noreply.github.com>
2025-03-29 17:37:16 -0300

    :sparkles: feat: created DocConstructor to handle docs generation

---
+++ addons/godocs/doc_constructor.gd
@@ -0,0 +1,38 @@
+
+class_name DocConstructor
+extends RefCounted
+
+var build_path: String
+
+var build_format: String
+
+var _builders: Array[DocBuilder] = []
+
+var _doc_name: String = ""
+
+func _init(
+	build_path: String,
+	build_format: String = ".txt"
+) -> void:
+	self.build_path = build_path
+	self.build_format = build_format
+
+func construct(db: ClassDocDB) -> Error:
+	var output: String = DocBuilder.build(db, _builders)
+	
+	return write_to_file(build_path.path_join(_doc_name), output)
+
+func write_to_file(path: String, content: String) -> Error:
+	if not DirAccess.dir_exists_absolute(path):
+		return ERR_FILE_NOT_FOUND
+	
+	var file: FileAccess = FileAccess.open(path, FileAccess.WRITE)
+	
+	if file == null:
+		return file.get_open_error()
+	
+	file.store_string(content)
+	
+	file.close()
+	
+	return OK

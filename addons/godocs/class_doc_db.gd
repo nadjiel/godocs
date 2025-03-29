6e125dbbf1ae4349a169c77e98688dae426fe102
Daniel de Oliveira <97067805+nadjiel@users.noreply.github.com>
2025-03-29 18:14:38 -0300

    :sparkles: feat(ClassDocDB): now has methods to get class inheritage

--- addons/godocs/class_doc_db.gd
+++ addons/godocs/class_doc_db.gd
@@ -4,6 +4,8 @@ extends RefCounted
 
 var data: Array[XMLDocument] = []
 
+var current_class: String = ""
+
 func get_class_document_by_idx(idx: int) -> XMLDocument:
 	if idx >= 0 and idx < data.size():
 		return data[idx]
@@ -18,8 +20,30 @@ func get_class_document(name: String) -> XMLDocument:
 	
 	return get_class_document_by_idx(idx)
 
+func get_current_class_document() -> XMLDocument:
+	return get_class_document(current_class)
+
 func set_class_document(document: XMLDocument) -> void:
 	data.append(document)
 
 func get_class_document_list() -> Array[XMLDocument]:
 	return data
+
+func get_class_inheritage(
+	descendent_name: String = current_class,
+	inheritage: Array[String] = []
+) -> Array[String]:
+	var document: XMLDocument = get_class_document(descendent_name)
+	
+	if document == null:
+		return inheritage
+	
+	var descendent_node: XMLNode = document.root
+	var parent_name: String = descendent_node.attributes.get("inherits", "")
+	
+	if parent_name == "":
+		return inheritage
+	
+	inheritage.append(parent_name)
+	
+	return get_class_inheritage(parent_name, inheritage)

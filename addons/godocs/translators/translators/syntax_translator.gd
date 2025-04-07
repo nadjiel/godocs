874884e21217625859f7f8efcb3daa86f6026973
Daniel de Oliveira <97067805+nadjiel@users.noreply.github.com>
2025-04-07 15:28:03 -0300

    :sparkles: feat: created the SyntaxTranslator

---
+++ addons/godocs/translators/translators/syntax_translator.gd
@@ -0,0 +1,9 @@
+
+class_name SyntaxTranslator
+extends RefCounted
+
+func translate_text(node: AbstractSyntaxTextNode) -> String:
+	return ""
+
+func translate_tag(node: AbstractSyntaxTagNode) -> String:
+	return ""

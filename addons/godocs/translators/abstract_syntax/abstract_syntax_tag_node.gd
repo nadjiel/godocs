bc5aec35af61d95b75d60c27c3630dede8d02c6d
Daniel de Oliveira <97067805+nadjiel@users.noreply.github.com>
2025-04-07 15:26:33 -0300

    :sparkles: feat: created the AbstractSyntaxTagNode

---
+++ addons/godocs/translators/abstract_syntax/abstract_syntax_tag_node.gd
@@ -0,0 +1,37 @@
+
+class_name AbstractSyntaxTagNode
+extends AbstractSyntaxNode
+
+var name: String = ""
+
+var params: Dictionary[String, String] = {}
+
+var children: Array[AbstractSyntaxNode] = []
+
+func _init(
+	name: String,
+	children: Array[AbstractSyntaxNode] = [],
+	params: Dictionary[String, String] = {},
+) -> void:
+	self.name = name
+	self.children = children
+	self.params = params
+
+func _to_string() -> String:
+	var result: String = "<%s" % self.name
+	
+	if not children.is_empty():
+		result += "\n%s\n" % stringify_children().indent("\t")
+	
+	result += ">"
+	
+	return result
+
+func stringify_children() -> String:
+	return children.reduce((
+		func(prev: String, next: AbstractSyntaxNode) -> String:
+			if prev == "":
+				return str(next)
+			
+			return prev + ",\n" + str(next)
+	), "")

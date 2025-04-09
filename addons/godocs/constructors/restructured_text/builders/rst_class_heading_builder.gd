0a71051753338b71d0bd51dba7ca7afff3163874
Daniel de Oliveira <97067805+nadjiel@users.noreply.github.com>
2025-04-09 16:50:07 -0300

    :recycle: refac(RSTClassHeadingBuilder): to use newer methods

--- addons/godocs/constructors/restructured_text/builders/rst_class_heading_builder.gd
+++ addons/godocs/constructors/restructured_text/builders/rst_class_heading_builder.gd
@@ -8,8 +8,7 @@ func make_inheritage_output(db: ClassDocDB) -> String:
 	var result: String = ""
 	
 	for parent: String in inheritage:
-		var parent_output: String = parse_class_name(parent)
-		parent_output = make_ref(parent, "class_" + parent_output)
+		var parent_output: String = make_class_ref(parent)
 		
 		if result == "":
 			result = "%s %s" % [ make_bold("Inherits:"), parent_output ]
@@ -21,22 +20,24 @@ func make_inheritage_output(db: ClassDocDB) -> String:
 func _build(db: ClassDocDB) -> String:
 	var document: XMLDocument = db.get_current_class_document()
 	var class_node: XMLNode = document.root
-	var brief_description_node := class_node.get_child_by_name(
+	var brief_description_node: XMLNode = class_node.get_child_by_name(
 		"brief_description"
-	) as XMLNode
+	)
 	
 	var name := class_node.attributes.get("name", "") as String
 	var name_size: int = name.length()
 	var brief_description: String = brief_description_node.content
 	
-	var label_output: String = make_label("class_%s" % parse_class_name(name))
+	var label_output: String = make_class_label(name)
+	var overline_output: String = "=".repeat(name_size)
 	var title_output: String = name
 	var underline_output: String = "=".repeat(name_size)
 	var inheritage_output: String = make_inheritage_output(db)
 	var brief_description_output: String = brief_description
 	
-	var result: String = "\n%s\n\n%s\n%s\n\n%s\n" % [
+	var result: String = "\n%s\n\n%s\n%s\n%s\n\n%s\n" % [
 		label_output,
+		overline_output,
 		title_output,
 		underline_output,
 		inheritage_output

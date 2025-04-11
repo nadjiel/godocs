572dc73a41b53a8d64827e127c6c3f642a7aff25
Daniel de Oliveira <97067805+nadjiel@users.noreply.github.com>
2025-04-11 19:00:14 -0300

    :test_tube: test(RSTDocBuilder): created manual test for maker methods

---
+++ test/manual/constructors/rst/builders/rst_doc_builder/test_rst_doc_builder.gd
@@ -0,0 +1,23 @@
+
+extends Node
+
+func _ready() -> void:
+	print_rich("[b]BOLD:[/b] ", RSTDocBuilder.make_bold("bold"))
+	print_rich("[b]ITALICS:[/b] ", RSTDocBuilder.make_italics("italics"))
+	print_rich("[b]CODE:[/b] ", RSTDocBuilder.make_code("code"))
+	print_rich("[b]LINK:[/b] ", RSTDocBuilder.make_link("https://github.com/nadjiel/godocs", "Godocs"))
+	print_rich("[b]COMMENT:[/b] ", RSTDocBuilder.make_comment("comment"))
+	print_rich("[b]REFERENCE:[/b] ", RSTDocBuilder.make_ref("Reference", "to_this"))
+	print_rich("[b]LABEL:[/b] ", RSTDocBuilder.make_label("label"))
+	print_rich("[b]CODE_MEMBER:[/b] ", RSTDocBuilder.parse_code_member_name("OuterClass.InnerClass.Enum.CONSTANT"))
+	print_rich("[b]CODE_MEMBER_REFERENCE:[/b] ", RSTDocBuilder.make_code_member_ref("OuterClass.InnerClass.Enum.CONSTANT", "CONSTANT"))
+	print_rich("[b]CODE_MEMBER_LABEL:[/b] ", RSTDocBuilder.make_code_member_label("OuterClass.InnerClass.Enum.CONSTANT"))
+	print_rich("[b]CODE_MEMBER_TYPE:[/b] ", RSTDocBuilder.parse_code_member_type("OuterClass.InnerClass.Enum[]"))
+	print_rich("[b]CODE_MEMBER_TYPE_REFERENCE:[/b] ", RSTDocBuilder.make_code_member_type_ref("OuterClass.InnerClass.Enum[]"))
+	print_rich("[b]PROPERTY_SIGNATURE:[/b] ", RSTDocBuilder.make_property_signature("property", "Array", "[]"))
+	print_rich("[b]METHOD_SIGNATURE:[/b] ", RSTDocBuilder.make_method_signature("method", "Array", [
+		{ "name": "param", "type": "int", "default": "0" }
+	]))
+	print_rich("[b]CODE_BLOCK:[/b]\n", RSTDocBuilder.make_codeblock('print("Hello, World!")', "GDScript"))
+	print_rich("[b]TABLE:[/b]\n", RSTDocBuilder.make_table([ [ "1", "2" ], [ "3", "4" ] ]))
+	

1ab7e4cb4317476141e46564f4014ec2c9af2a93
Daniel de Oliveira <97067805+nadjiel@users.noreply.github.com>
2025-04-11 16:11:24 -0300

    :test_tube: test(DocParser): manual test created

---
+++ test/manual/doc_parser/test_doc_parser.gd
@@ -0,0 +1,10 @@
+
+extends Node
+
+var parser := DocParser.new("res://docs/")
+
+func _ready() -> void:
+	prints("Result:", error_string(parser.parse()), "\n")
+	
+	for doc: XMLDocument in parser.db.get_class_document_list():
+		print(doc.root.attributes.get("name", "NONAME"))

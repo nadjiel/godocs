abc1f18cadf64cab8b710c720e701f4ab93eb50f
Daniel de Oliveira <97067805+nadjiel@users.noreply.github.com>
2025-04-12 10:40:52 -0300

    :test_tube: test(RSTDocBuilder): added manual test to block comment makers

--- test/manual/constructors/rst/builders/rst_doc_builder/test_rst_doc_builder.gd
+++ test/manual/constructors/rst/builders/rst_doc_builder/test_rst_doc_builder.gd
@@ -6,7 +6,7 @@ func _ready() -> void:
 	print_rich("[b]ITALICS:[/b] ", RSTDocBuilder.make_italics("italics"))
 	print_rich("[b]CODE:[/b] ", RSTDocBuilder.make_code("code"))
 	print_rich("[b]LINK:[/b] ", RSTDocBuilder.make_link("https://github.com/nadjiel/godocs", "Godocs"))
-	print_rich("[b]COMMENT:[/b] ", RSTDocBuilder.make_comment("comment"))
+	print_rich("[b]COMMENT:[/b] ", RSTDocBuilder.make_comment("Comment"))
 	print_rich("[b]REFERENCE:[/b] ", RSTDocBuilder.make_ref("Reference", "to_this"))
 	print_rich("[b]LABEL:[/b] ", RSTDocBuilder.make_label("label"))
 	print_rich("[b]CODE_MEMBER:[/b] ", RSTDocBuilder.parse_code_member_name("OuterClass.InnerClass.Enum.CONSTANT"))
@@ -18,6 +18,6 @@ func _ready() -> void:
 	print_rich("[b]METHOD_SIGNATURE:[/b] ", RSTDocBuilder.make_method_signature("method", "Array", [
 		{ "name": "param", "type": "int", "default": "0" }
 	]))
+	print_rich("[b]COMMENT_BLOCK:[/b]\n", RSTDocBuilder.make_comment_block("Long comment"))
 	print_rich("[b]CODE_BLOCK:[/b]\n", RSTDocBuilder.make_codeblock('print("Hello, World!")', "GDScript"))
 	print_rich("[b]TABLE:[/b]\n", RSTDocBuilder.make_table([ [ "1", "2" ], [ "3", "4" ] ]))
-	

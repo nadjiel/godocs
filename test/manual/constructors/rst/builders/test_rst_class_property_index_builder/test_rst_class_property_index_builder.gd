276fbf9074c47beb91814b8510b11525342b44c8
Daniel de Oliveira <97067805+nadjiel@users.noreply.github.com>
2025-04-12 14:51:43 -0300

    :test_tube: test(RSTClassPropertyIndexBuilder): created manual test

---
+++ test/manual/constructors/rst/builders/test_rst_class_property_index_builder/test_rst_class_property_index_builder.gd
@@ -0,0 +1,13 @@
+
+extends Node
+
+var parser := DocParser.new("res://test/docs/xml/GodocsCLI.xml")
+
+var builder: DocBuilder = RSTClassPropertyIndexBuilder.new()
+
+func _ready() -> void:
+	parser.parse()
+	
+	parser.db.current_class = "GodocsCLI"
+	
+	prints("Result:\n", builder.build(parser.db))

cf3b0c0f5c4c106f2e013d1d61e17038de256da2
Daniel de Oliveira <97067805+nadjiel@users.noreply.github.com>
2025-04-14 17:56:05 -0300

    :sparkles: feat: created RSTClassIndexBuilder

---
+++ addons/godocs/constructors/rst/builders/rst_class_index_builder.gd
@@ -0,0 +1,22 @@
+
+class_name RSTClassIndexBuilder
+extends RSTDocBuilder
+
+func _build_class_list(db: ClassDocDB) -> String:
+	var result: String = make_toctree({
+		"maxdepth": "1",
+		"glob": "",
+	}, [ "*" ])
+	
+	return result
+
+func build(db: ClassDocDB) -> String:
+	var title_output: String = make_heading("Class Index", 1)
+	var list_output: String = _build_class_list(db)
+	
+	var result: String = "\n%s\n\n%s\n" % [
+		title_output,
+		list_output
+	]
+	
+	return result

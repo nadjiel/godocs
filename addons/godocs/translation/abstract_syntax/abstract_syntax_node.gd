e54e05212d4278f37178929240d03fe7ac7b2bd1
Daniel de Oliveira <97067805+nadjiel@users.noreply.github.com>
2025-04-15 15:59:46 -0300

    :books: docs(AbstractSyntaxNode): added description

--- addons/godocs/translation/abstract_syntax/abstract_syntax_node.gd
+++ addons/godocs/translation/abstract_syntax/abstract_syntax_node.gd
@@ -1,9 +1,18 @@
-
+## [AbstractSyntaxNode] models a node used to represent an Abstract Syntax.
+## 
+## This class is the basic piece of data that constructs an
+## [b]Abstract Syntax Tree[/b] (AST).[br]
+## To represent different kinds of information, this class is extended by the
+## [AbstractSyntaxTextNode] and [AbstractSyntaxTagNode] classes.
 class_name AbstractSyntaxNode
 extends RefCounted
 
 func _to_string() -> String:
 	return "<>"
 
+## The [method translate] method can be used with the help of a
+## [SyntaxTranslator] to transform the [b]Abstract Syntax[/b]
+## represented by this AST, into a [b]Concrete Syntax[/b].[br]
+## [i]See also [SyntaxTranslator][/i].
 func translate(translator: SyntaxTranslator) -> String:
 	return ""

58e3e230dbcd8a863607535444d59e3c4442b6fa
Daniel de Oliveira <97067805+nadjiel@users.noreply.github.com>
2025-04-15 16:00:09 -0300

    :books: docs(AbstractSyntaxTextNode): added description

--- addons/godocs/translation/abstract_syntax/abstract_syntax_text_node.gd
+++ addons/godocs/translation/abstract_syntax/abstract_syntax_text_node.gd
@@ -1,7 +1,16 @@
-
+## [AbstractSyntaxTextNode] represents a component of an AST that stores
+## bare text.
+## 
+## This class is used to represent a [b]basic[/b] text [String] without any
+## additional informations associated.[br]
+## In order for this class to be instantiated it needs a [param content], which
+## is stored as the text it represents.[br]
+## [i]See also [AbstractSyntaxNode][/i] 
 class_name AbstractSyntaxTextNode
 extends AbstractSyntaxNode
 
+## The [member content] property stores the [b]text[/b] associated with
+## this node.
 var content: String = ""
 
 func _init(content: String) -> void:
@@ -10,5 +19,9 @@ func _init(content: String) -> void:
 func _to_string() -> String:
 	return '"%s"' % content
 
+## The [method translate] method can be used with the help of a
+## [SyntaxTranslator] implementation to obtain the equivalent [b]translation[/b]
+## of the [member content] of this node in that [b]other syntax[/b].[br]
+## [i]See also [SyntaxTranslator][/i].
 func translate(translator: SyntaxTranslator) -> String:
 	return translator.translate_text(self)

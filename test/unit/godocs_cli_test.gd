8804e8f565cf26c3497adea54a0fa50e319384e9
Daniel de Oliveira <97067805+nadjiel@users.noreply.github.com>
2025-04-15 13:57:14 -0300

    :test_tube: test(unit/GodocsCLI): fixed leaked objects

--- test/unit/godocs_cli_test.gd
+++ test/unit/godocs_cli_test.gd
@@ -4,32 +4,38 @@ extends GdUnitTestSuite
 var godocs: GodocsCLI
 
 func test_godocs_parses_key_value_arguments() -> void:
-	godocs = auto_free(GodocsCLI.new()) as GodocsCLI
-	godocs.parse_args([
+	godocs = GodocsCLI.new()
+	godocs._parse_args([
 		"src=src_path",
 		"build=build_path",
 	])
 	
-	var args: Dictionary = godocs.args
+	var args: Dictionary = godocs._args
 	
 	assert_dict(args)\
 		.contains_key_value("src", "src_path")\
 		.contains_key_value("build", "build_path")
+	
+	godocs.free()
 
 func test_godocs_parses_constructor_type_to_enum() -> void:
-	godocs = auto_free(GodocsCLI.new()) as GodocsCLI
+	godocs = GodocsCLI.new()
 	
-	var result: Godocs.ConstructorType = godocs._parse_constructor_type("RST")
+	var result := godocs._parse_constructor_type("RST") as Godocs.ConstructorType
 	
 	assert_that(result)\
 		.is_equal(Godocs.ConstructorType.RST)
+	
+	godocs.free()
 
 func test_godocs_parses_constructor_type_to_path() -> void:
-	godocs = auto_free(GodocsCLI.new()) as GodocsCLI
+	godocs = GodocsCLI.new()
 	
 	var expected: String = "addons/godocs/constructors/rst/rst_doc_constructor.gd"
 	
-	var result: Godocs.ConstructorType = godocs._parse_constructor_type(expected)
+	var result := godocs._parse_constructor_type(expected) as String
 	
 	assert_that(result)\
 		.is_equal(expected)
+	
+	godocs.free()

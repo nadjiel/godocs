c01495cbab36c9ff37c3a4b8d5cfc957d08f0054
Daniel de Oliveira <97067805+nadjiel@users.noreply.github.com>
2025-04-15 17:49:12 -0300

    :books: docs(RSTSyntaxTranslator): added description

--- addons/godocs/translation/translators/rst_syntax_translator.gd
+++ addons/godocs/translation/translators/rst_syntax_translator.gd
@@ -1,16 +1,41 @@
-
+## Transforms an [b]Abstract Syntax Tree[/b] into a
+## [b]RestructuredText[/b] [String].
+## 
+## The [RSTSyntaxTranslator] class can translate an abstract representation
+## of [AbstractSyntaxNode]s into a text written in
+## [b]Restructured Text[/b] (RST).[br]
+## To do that, this class extends the [SyntaxTranslator] class and adds
+## its own logic to it.[br]
+## [i]See also: [SyntaxTranslator] and [AbstractSyntaxNode][/i]
 class_name RSTSyntaxTranslator
 extends SyntaxTranslator
 
+## The [method translate_text] method [b]overrides[/b] its parent
+## [method SyntaxTranslator.translate_text]
+## in order to define how this Translator performs the
+## [b]parsing[/b] of an [AbstractSyntaxTextNode] to a text with
+## [b]RST[/b] syntax.[br]
+## To realize that parsing, this method just returns the
+## [member AbstractSyntaxTextNode.content], as it's only that that
+## that kind of Node represents.[br]
+## [i]See also: [AbstractSyntaxTextNode][/i]
 func translate_text(node: AbstractSyntaxTextNode) -> String:
 	return node.content
 
+## The [method translate_tag] method [b]overrides[/b] its parent
+## [method SyntaxTranslator.translate_tag]
+## in order to define how this Translator performs the
+## [b]parsing[/b] of an [AbstractSyntaxTagNode] to a text with
+## [b]RST[/b] syntax.[br]
+## [i]See also: [AbstractSyntaxTagNode][/i]
 func translate_tag(node: AbstractSyntaxTagNode) -> String:
+	# First of all, translates the children of the node received.
 	var content: String = node.children.reduce((
 		func(prev: String, next: AbstractSyntaxNode) -> String:
 			return prev + next.translate(self)
 	), "")
 	
+	# Depending on the node name, the resultant syntax will change.
 	match node.name:
 		"root": return content
 		"bold": return RSTDocBuilder.make_bold(content)

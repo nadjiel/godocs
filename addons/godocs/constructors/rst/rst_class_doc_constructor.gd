31c4e0faa86c4c8a32439ac8f4773d8ebac4e1af
Daniel de Oliveira <97067805+nadjiel@users.noreply.github.com>
2025-04-16 17:30:41 -0300

    :books: docs(RSTClassDocConstructor): added description

--- addons/godocs/constructors/rst/rst_class_doc_constructor.gd
+++ addons/godocs/constructors/rst/rst_class_doc_constructor.gd
@@ -1,4 +1,17 @@
-
+## The [RSTClassDocConstructor] class creates a set of documents describing
+## classes from a whole code base.
+## 
+## With this class, [b]Restructured Text[/b] (RST) documentation can be
+## generated and saved for [b]multiple classes[/b] in a code base.[br]
+## In order to do that, this class first needs to be created with a
+## [param build_path] where it can later save its output.[br]
+## The documentation created by this class includes one [b]file for each known
+## class[/b].[br]
+## The structure of the generated documents is defined, respectively, by
+## the [RSTClassHeadingBuilder], [RSTClassDescriptionBuilder],
+## [RSTClassPropertyIndexBuilder], [RSTClassMethodIndexBuilder],
+## [RSTClassPropertyDescriptionBuilder] and [RSTClassMethodDescriptionBuilder]
+## builders. See them for details about each part of the structure.
 class_name RSTClassDocConstructor
 extends ClassDocConstructor
 

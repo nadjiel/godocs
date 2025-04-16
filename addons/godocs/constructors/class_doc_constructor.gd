d79f28366e70888119423a9f156c0937926f5281
Daniel de Oliveira <97067805+nadjiel@users.noreply.github.com>
2025-04-16 16:57:17 -0300

    :books: docs(ClassDocConstructor): added description

--- addons/godocs/constructors/class_doc_constructor.gd
+++ addons/godocs/constructors/class_doc_constructor.gd
@@ -1,7 +1,25 @@
-
+## Helps with the creation of documentation for multiple classes.
+## 
+## The [ClassDocConstructor] is a specialized [b]implementation[/b] of the
+## [DocConstructor] that can be used to create [b]documentation for multiple
+## classes[/b] at once using [b]a single mold[/b] (described by its
+## [DocBuilder]s).[br]
+## [i]See also: [DocConstructor] and [DocBuilder].[/i]
 class_name ClassDocConstructor
 extends DocConstructor
 
+## The [method construct] method overrides its parent
+## [method DocConstructor.construct] in order to make this class capable of
+## [b]creating multiple output documents[/b] with only one call to it.[br]
+## Each generated document is [b]named after the class[/b] on which it was based,
+## unless when these names include invalid characters (such as
+## [code]"/"[/code], because file names can't include them), in which case,
+## they aren't even created (invalid class names can appear when trying to
+## generate documentation from classes that don't have a defined
+## [code]class_name[/code].[br]
+## Just like its super counterpart, this method also needs a [ClassDocDB]
+## instance in order to know the classes existent in the code base.[br]
+## [i]See also: [method DocConstructor.construct].[/i]
 func construct(db: ClassDocDB) -> Error:
 	var class_documents: Array[XMLDocument] = db.get_class_document_list()
 	

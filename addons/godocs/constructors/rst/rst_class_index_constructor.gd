37a26997db88c6142965ab92b17f74ad403a4260
Daniel de Oliveira <97067805+nadjiel@users.noreply.github.com>
2025-04-16 17:26:46 -0300

    :books: docs(RSTClassIndexConstructor): added description

--- addons/godocs/constructors/rst/rst_class_index_constructor.gd
+++ addons/godocs/constructors/rst/rst_class_index_constructor.gd
@@ -1,4 +1,14 @@
-
+## The [RSTClassIndexConstructor] class creates a document listing the
+## classes present in a code base.
+## 
+## With this class, a [b]Restructured Text[/b] (RST) document listing
+## [b]multiple classes[/b] in a code base can be generated and saved.[br]
+## In order to do that, this class first needs to be created with a
+## [param build_path] where it can later save its output.[br]
+## The documentat created by this class is called [code]index.rst[/code].[br]
+## To create that document, this class uses the [RSTClassIndexBuilder], which
+## defines its resultant structure.[br]
+## [i]See also [RSTClassIndexBuilder][/i]
 class_name RSTClassIndexConstructor
 extends DocConstructor
 

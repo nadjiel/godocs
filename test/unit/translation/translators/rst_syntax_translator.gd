bff0dca6ea5710677cea616045a155cdd90425d2
Daniel de Oliveira <97067805+nadjiel@users.noreply.github.com>
2025-04-16 15:24:27 -0300

    :test_tube: test(RSTSyntaxTranslator): added more unit tests

--- test/unit/translation/translators/rst_syntax_translator.gd
+++ test/unit/translation/translators/rst_syntax_translator.gd
@@ -59,3 +59,59 @@ func test__make_table_content_works_with_square_matrix() -> void:
 | c | d |
 +---+---+
 ")
+
+func test_normalize_code_member_works_with_full_names() -> void:
+	var full_name: String = "Class.InnerClass.Enum.CONSTANT"
+	
+	assert_str(RSTSyntaxTranslator._normalize_code_member(full_name))\
+		.is_equal("Class_InnerClass_Enum_CONSTANT")
+
+func test_normalize_code_member_works_with_array_types() -> void:
+	var type_name: String = "Class.InnerClass.Enum[]"
+	
+	assert_str(RSTSyntaxTranslator._normalize_code_member(type_name))\
+		.is_equal("Array[Class_InnerClass_Enum]")
+
+func test_normalize_code_member_works_with_dict_types() -> void:
+	var type_name: String = "Dictionary[Class.InnerClass.Enum, String]"
+	
+	assert_str(RSTSyntaxTranslator._normalize_code_member(type_name))\
+		.is_equal("Dictionary[Class_InnerClass_Enum, String]")
+
+func test_make_code_member_label_target_works_with_full_names() -> void:
+	var full_name: String = "Class.InnerClass.Enum.CONSTANT"
+	
+	assert_str(RSTSyntaxTranslator._make_code_member_label_target(full_name))\
+		.is_equal("godocs_Class_InnerClass_Enum_CONSTANT")
+
+func test_make_code_member_label_target_works_with_short_names() -> void:
+	var short_name: String = "CONSTANT"
+	
+	assert_str(RSTSyntaxTranslator._make_code_member_label_target(short_name))\
+		.is_equal("godocs_CONSTANT")
+
+func test_make_code_member_label_works_with_full_names() -> void:
+	var full_name: String = "Class.InnerClass.Enum.CONSTANT"
+	
+	assert_str(RSTSyntaxTranslator.make_code_member_label(full_name))\
+		.is_equal(".. _godocs_Class_InnerClass_Enum_CONSTANT:")
+
+func test_make_code_member_label_works_with_short_names() -> void:
+	var short_name: String = "CONSTANT"
+	
+	assert_str(RSTSyntaxTranslator.make_code_member_label(short_name))\
+		.is_equal(".. _godocs_CONSTANT:")
+
+func test_make_code_member_ref_works_with_full_names() -> void:
+	var full_name: String = "Class.InnerClass.Enum.CONSTANT"
+	
+	assert_str(RSTSyntaxTranslator.make_code_member_ref(full_name, "CONSTANT"))\
+		.is_equal(":ref:`CONSTANT <godocs_Class_InnerClass_Enum_CONSTANT>`")
+
+func test_make_code_member_type_ref_works_with_type_names() -> void:
+	var type_name: String = "Dictionary[Class.InnerClass.Enum, String]"
+	
+	print(RSTSyntaxTranslator.make_code_member_type_ref(type_name))
+	
+	assert_str(RSTSyntaxTranslator.make_code_member_type_ref(type_name))\
+		.is_equal(":ref:`Dictionary <godocs_Dictionary>`[:ref:`Class_InnerClass_Enum <godocs_Class_InnerClass_Enum>`, :ref:`String <godocs_String>`]")

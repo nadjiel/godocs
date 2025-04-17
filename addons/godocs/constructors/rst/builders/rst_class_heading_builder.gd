3a665952a2d749878cb9b241be56e4b51eac5abd
Daniel de Oliveira <97067805+nadjiel@users.noreply.github.com>
2025-04-17 13:01:59 -0300

    :books: docs(RSTClassHeadingBuilder): added description

--- addons/godocs/constructors/rst/builders/rst_class_heading_builder.gd
+++ addons/godocs/constructors/rst/builders/rst_class_heading_builder.gd
@@ -1,8 +1,12 @@
-
+## Creates a RST heading describing a class.
+## 
+## The [RSTClassHeadingBuilder] is capable of creating a [String] with
+## Restructured Text syntax describing a class stored in a [ClassDocDB].[br]
+## [i]See also: [RSTDocBuilder] and [ClassDocDB].[/i]
 class_name RSTClassHeadingBuilder
 extends RSTDocBuilder
 
-func make_inheritage_output(db: ClassDocDB) -> String:
+func _make_inheritage_output(db: ClassDocDB) -> String:
 	var inheritage: Array[String] = db.get_class_inheritage()
 	
 	var result: String = ""
@@ -17,32 +21,49 @@ func make_inheritage_output(db: ClassDocDB) -> String:
 	
 	return result
 
+## The [method build] method uses the information contained in a [ClassDocDB]
+## passed via the [param db] parameter to build a [b]heading[/b]
+## for the documentation of the [b]class pointed by it[/b].[br]
+## The format of the output of this method is the following:
+## [codeblock lang=rst]
+## 
+## .. _godocs_Class:
+## 
+## =====
+## Class
+## =====
+## 
+## **Inherits:** :ref:`SuperClass <godocs_SuperClass>` **<** :ref:`RefCounted <godocs_RefCounted>`
+## 
+## User created brief description.
+## 
+## [/codeblock]
 func build(db: ClassDocDB) -> String:
 	var bbcode: SyntaxInterpreter = BBCodeSyntaxInterpreter.new()
 	var rst: SyntaxTranslator = RSTSyntaxTranslator.new()
 	
-	var document: XMLDocument = db.get_current_class_document()
+	var doc: XMLDocument = db.get_current_class_document()
 	
-	if document == null:
+	if doc == null:
 		return ""
 	
-	var class_node: XMLNode = document.root
+	var class_node: XMLNode = doc.root
 	var brief_description_node: XMLNode = class_node.get_child_by_name(
 		"brief_description"
 	)
 	
-	var name := class_node.attributes.get("name", "") as String
+	var name: String = class_node.attributes.get("name", "")
 	var brief_description: String = brief_description_node.content
 	
 	var label_output: String = RSTSyntaxTranslator.make_code_member_label(name)
 	var title_output: String = RSTSyntaxTranslator.make_heading(name, 1)
-	var inheritage_output: String = make_inheritage_output(db)
-	var brief_description_output: String = ( bbcode
-		.interpret(brief_description)
-		.translate(rst) )
+	var inheritage_output: String = _make_inheritage_output(db)
+	var brief_description_output: String = bbcode\
+		.interpret(brief_description)\
+		.translate(rst)
 	brief_description_output = autocomplete_code_member_refs(
 		brief_description_output,
-		db
+		db,
 	)
 	
 	var result: String = "\n%s\n\n%s\n%s\n" % [
